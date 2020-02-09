3f0391c37efc4d0773157e11af366c8005d59f17
Gautier <gautier2406@gmail.com>
2020-02-09 17:20:54 +0100

    App: Add Theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_dingo/src/screens/log_in.dart';
+
+const Color mainTheme = Color.fromRGBO(38, 45, 64, 1);
 
 class MyApp extends StatelessWidget {
   @override
@@ -6,9 +9,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tempo Dingo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: mainTheme,
+        fontFamily: 'Apple',
+        textTheme: TextTheme(
+          headline: TextStyle(
+            color: Colors.white,
+            fontSize: 45,
+            fontFamily: 'Apple',
+            fontWeight: FontWeight.bold,
+          ),
+          title: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontFamily: 'Apple-Semibold',
+            fontWeight: FontWeight.bold,
+          ),
+          body1: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontFamily: 'Apple',
+          ),
+        ),
       ),
-      home: Container(),
+      home: Scaffold(
+        backgroundColor: mainTheme,
+        body: LogIn(),
+      ),
     );
   }
 }
