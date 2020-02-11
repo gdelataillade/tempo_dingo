8d63e0beeea4ca6550be19e14e4f5e2936856731
Gautier <gautier2406@gmail.com>
2020-02-11 17:18:23 +0100

    Register: Add config

---
+++ lib/src/config/register_config.dart
@@ -0,0 +1,10 @@
+final int stars = 10;
+final String language = "EN";
+final bool staySignedIn = false;
+final bool vibration = true;
+final bool darkTheme = true;
+final Map<String, List<int>> library = {
+  "songs": [],
+  "artists": [],
+  "favorite": [],
+};
