cbb895cf748b0575f84c7b3890072da7722a17e8
Gautier <gautier2406@gmail.com>
2020-02-12 15:42:18 +0100

    Config: Add default library when register

--- lib/src/config/register_config.dart
+++ lib/src/config/register_config.dart
@@ -3,8 +3,16 @@ final String language = "EN";
 final bool staySignedIn = false;
 final bool vibration = true;
 final bool darkTheme = true;
-final Map<String, List<int>> library = {
-  "songs": [],
-  "artists": [],
-  "favorite": [],
+final Map<String, List<String>> library = {
+  "songs": [
+    "2aoo2jlRnM3A0NyLQqMN2f", // All Along the Watchtower
+    "45KqDHTOJtIM53QR1gPGTa", // Brothers
+  ],
+  "artists": [
+    "776Uo845nYHJpNaStv1Ds4", // Jimi Hendrix
+    "6pdcQa7by8IKuoVXvgknlI", // Riles
+  ],
+  "favorite": [
+    "2aoo2jlRnM3A0NyLQqMN2f", // All Along the Watchtower
+  ],
 };
