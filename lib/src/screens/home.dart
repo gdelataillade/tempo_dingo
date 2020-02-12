662902ccfb82809cf9c27225e89286150a226420
Gautier <gautier2406@gmail.com>
2020-02-12 14:41:17 +0100

    Home: Add architecture

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -15,10 +15,47 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: mainTheme,
       body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Tempo Dingo",
+              style: Theme.of(context).textTheme.headline,
+            ),
+            _Artists(),
+            _QuickPlay(),
+          ],
         ),
       ),
     );
+  }
+}
+
+class _Artists extends StatefulWidget {
+  _Artists({Key key}) : super(key: key);
+
+  @override
+  __ArtistsState createState() => __ArtistsState();
+}
+
+class __ArtistsState extends State<_Artists> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _QuickPlay extends StatefulWidget {
+  _QuickPlay({Key key}) : super(key: key);
+
+  @override
+  __QuickPlayState createState() => __QuickPlayState();
+}
+
+class __QuickPlayState extends State<_QuickPlay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
