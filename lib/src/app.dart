e29ff6ebd3935ad22d78eac4b7774671fa72ebb6
Gautier <gautier2406@gmail.com>
2020-02-13 12:44:51 +0100

    App: Move theme to theme_config.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,34 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tempo Dingo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: mainTheme,
-        fontFamily: 'Apple',
-        textTheme: TextTheme(
-          headline: TextStyle(
-            color: Colors.white,
-            fontSize: 45,
-            fontFamily: 'Apple-Bold',
-            fontWeight: FontWeight.w700,
-          ),
-          title: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontFamily: 'Apple-Semibold',
-            fontWeight: FontWeight.bold,
-          ),
-          body1: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontFamily: 'Apple',
-          ),
-          body2: TextStyle(
-            color: Colors.white,
-            fontSize: 14,
-            fontFamily: 'Apple',
-          ),
-        ),
-      ),
+      theme: themeConfig,
       home: Scaffold(
         backgroundColor: mainTheme,
         body: _CheckLogIn(),
