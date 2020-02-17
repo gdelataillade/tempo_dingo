fbf97b6e18d06b5beeae97b3102d2fe6d1347875
Gautier <gautier2406@gmail.com>
2020-02-17 17:20:22 +0100

    ArtistCard: Add shadow

--- lib/src/widgets/artist_card.dart
+++ lib/src/widgets/artist_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_dingo/src/config/theme_config.dart';
 
 class ArtistCard extends StatefulWidget {
   final String imgUrl;
@@ -22,6 +23,14 @@ class _ArtistCardState extends State<ArtistCard> {
             border: Border.all(color: Colors.white),
             color: Colors.white,
             borderRadius: BorderRadius.all(const Radius.circular(100)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 0.5,
+                blurRadius: 5,
+                offset: Offset(1, 1),
+              ),
+            ],
           ),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(50),
