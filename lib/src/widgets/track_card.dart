2957517ecf5f8d2545dbdb396f95c5d992d3df33
Gautier <gautier2406@gmail.com>
2020-02-25 13:21:45 +0100

    TrackCard: Fix text alignment

--- lib/src/widgets/track_card.dart
+++ lib/src/widgets/track_card.dart
@@ -53,7 +53,6 @@ class _TrackCardState extends State<TrackCard> {
           Padding(
             padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Text(
