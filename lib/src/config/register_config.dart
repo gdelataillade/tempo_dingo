c9b6ef171ed004111908e76b112461fc4656c9cf
Gautier <gautier2406@gmail.com>
2020-03-18 18:07:29 +0100

    Update default register library

--- lib/src/config/register_config.dart
+++ lib/src/config/register_config.dart
@@ -7,12 +7,14 @@ final Map<String, List<String>> library = {
   "songs": [
     "2aoo2jlRnM3A0NyLQqMN2f", // All Along the Watchtower
     "45KqDHTOJtIM53QR1gPGTa", // Brothers
+    "51R2M1JgyFfRS3e6v5wCt3", // Clandestino
+    "6FNdxaAOler5LutmU2sVxT", // L'hymne de nos campagnes 2019
   ],
   "artists": [
     "776Uo845nYHJpNaStv1Ds4", // Jimi Hendrix
     "6pdcQa7by8IKuoVXvgknlI", // Riles
+    "6wH6iStAh4KIaWfuhf0NYM", // Manu Chao
+    "4ZP3CHz28XWVLGiaVVWuLj", // Tryo
   ],
-  "favorite": [
-    "2aoo2jlRnM3A0NyLQqMN2f", // All Along the Watchtower
-  ],
+  "favorite": [],
 };
