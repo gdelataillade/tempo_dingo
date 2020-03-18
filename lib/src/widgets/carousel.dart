0397721d69d4900886073a0cedd0cb48fe8ed219
Gautier <gautier2406@gmail.com>
2020-03-18 18:09:19 +0100

    Carousel: Add album image

--- lib/src/widgets/carousel.dart
+++ lib/src/widgets/carousel.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:spotify/spotify_io.dart' as spotify;
 import 'package:tempo_dingo/src/models/user_model.dart';
 
 class Carousel extends StatefulWidget {
-  final List<String> tracksId;
+  final List<spotify.Track> tracks;
 
-  const Carousel(this.tracksId);
+  const Carousel(this.tracks);
 
   @override
   _CarouselState createState() => _CarouselState();
@@ -19,20 +20,43 @@ class _CarouselState extends State<Carousel> with TickerProviderStateMixin {
     return ScopedModelDescendant<UserModel>(
       builder: (context, child, model) {
         _userModel = model;
+        return Container(
+          height: 400,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20, right: 5),
+            itemCount: widget.tracks.length,
+            itemBuilder: (BuildContext context, int index) {
+              final spotify.Track track = widget.tracks[index];
 
-        // return FutureBuilder<>();
-
-        return Row(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              child: Column(
-                children: <Widget>[],
-              ),
-            ),
-          ],
+              return Container(
+                width: 250,
+                padding: const EdgeInsets.only(right: 15),
+                child: Column(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: Image.network(track.album.images.first.url,
+                          width: MediaQuery.of(context).size.width),
+                    )
+                  ],
+                ),
+              );
+              // return ListTile(
+              //   leading: Container(
+              //     width: 150,
+              //     height: 300,
+              //     decoration: BoxDecoration(
+              //       color: Colors.white,
+              //     ),
+              //     child: Row(
+              //       children: <Widget>[],
+              //     ),
+              //   ),
+              // );
+            },
+          ),
         );
       },
     );
