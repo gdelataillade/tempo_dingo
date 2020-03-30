f08f8f251c3bcfa35aea58fe113441678fb75b1c
Gautier <gautier2406@gmail.com>
2020-03-30 20:41:04 +0200

    Game: Add UI elements

--- lib/src/screens/game.dart
+++ lib/src/screens/game.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:spotify/spotify_io.dart';
+import 'package:spotify/spotify_io.dart' as spotify;
 import 'package:tempo_dingo/src/config/theme_config.dart';
 import 'package:tempo_dingo/src/models/user_model.dart';
 
 class Game extends StatefulWidget {
   final UserModel userModel;
-  final Track track;
+  final spotify.Track track;
 
   const Game(this.userModel, this.track);
 
@@ -34,10 +34,37 @@ class _GameState extends State<Game> {
               padding: const EdgeInsets.all(35),
               child: Column(
                 children: <Widget>[
-                  Text(_userModel.fullName,
-                      style: TextStyle(color: Colors.white)),
-                  Text(widget.track.name,
-                      style: TextStyle(color: Colors.white)),
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    child: Image.network(widget.track.album.images.first.url,
+                        width: MediaQuery.of(context).size.width),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.track.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontFamily: 'Apple-Bold',
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    widget.track.artists.first.name,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Container(
+                    height: 250,
+                    child: Center(
+                      child: Text(
+                        "Tap the screen",
+                        style: TextStyle(
+                          color: Color.fromRGBO(77, 83, 105, 0.5),
+                          fontFamily: 'Apple-Semibold',
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
