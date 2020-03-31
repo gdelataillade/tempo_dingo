4fcd68761257d482129309c9614a91db4e1a0b13
Gautier <gautier2406@gmail.com>
2020-03-31 15:55:58 +0200

    Game: Improve game button with MaterialButton

--- lib/src/screens/game.dart
+++ lib/src/screens/game.dart
@@ -16,6 +16,15 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game> {
   UserModel _userModel;
+  double _pourcentage;
+
+  @override
+  void initState() {
+    setState(() {
+      _pourcentage = 0.0;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,19 +35,13 @@ class _GameState extends State<Game> {
           _userModel = userModel;
           _userModel.addToHistory(widget.track.id);
           return Scaffold(
-            appBar: AppBar(
-              elevation: 1,
-            ),
+            appBar: AppBar(elevation: 1),
             backgroundColor: mainTheme,
             body: Padding(
               padding: const EdgeInsets.all(35),
               child: Column(
                 children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    child: Image.network(widget.track.album.images.first.url,
-                        width: MediaQuery.of(context).size.width),
-                  ),
+                  _AlbumProgressiveBar(widget.track, _pourcentage),
                   const SizedBox(height: 10),
                   Text(
                     widget.track.name,
@@ -53,23 +56,73 @@ class _GameState extends State<Game> {
                     widget.track.artists.first.name,
                     style: TextStyle(color: Colors.white),
                   ),
-                  Container(
-                    height: 250,
-                    child: Center(
-                      child: Text(
-                        "Tap the screen",
-                        style: TextStyle(
-                          color: Color.fromRGBO(77, 83, 105, 0.5),
-                          fontFamily: 'Apple-Semibold',
-                        ),
-                      ),
-                    ),
-                  ),
+                  const SizedBox(height: 10),
+                  _TapArea(),
                 ],
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _AlbumProgressiveBar extends StatefulWidget {
+  final spotify.Track track;
+  final double pourcentage;
+
+  const _AlbumProgressiveBar(this.track, this.pourcentage);
+
+  @override
+  __AlbumProgressiveBarState createState() => __AlbumProgressiveBarState();
+}
+
+class __AlbumProgressiveBarState extends State<_AlbumProgressiveBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        child: Image.network(widget.track.album.images.first.url,
+            width: MediaQuery.of(context).size.width),
+      ),
+    );
+  }
+}
+
+class _TapArea extends StatefulWidget {
+  const _TapArea();
+
+  @override
+  __TapAreaState createState() => __TapAreaState();
+}
+
+class __TapAreaState extends State<_TapArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 260,
+      width: 350,
+      child: Center(
+        child: MaterialButton(
+          height: 260,
+          minWidth: 350,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          onPressed: () => print("tap"),
+          child: Text(
+            "Tap the screen",
+            style: TextStyle(
+              color: Color.fromRGBO(77, 83, 105, 0.5),
+              fontFamily: 'Apple-Semibold',
+            ),
+          ),
+        ),
       ),
     );
   }
