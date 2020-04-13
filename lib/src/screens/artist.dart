3f7de4987ec9582f832d4b06a0b152cc720f91a5
Gautier <gautier2406@gmail.com>
2020-04-13 16:16:11 +0200

    Artist screen: Add artist image and name

--- lib/src/screens/artist.dart
+++ lib/src/screens/artist.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:spotify/spotify_io.dart';
+import 'package:spotify/spotify_io.dart' as spotify;
 import 'package:scoped_model/scoped_model.dart';
 import 'package:tempo_dingo/src/config/theme_config.dart';
 import 'package:tempo_dingo/src/models/user_model.dart';
 
 class ArtistScreen extends StatefulWidget {
   final UserModel userModel;
-  final Artist artist;
+  final spotify.Artist artist;
 
   const ArtistScreen(this.userModel, this.artist);
 
@@ -27,17 +27,16 @@ class _ArtistScreenState extends State<ArtistScreen> {
 
           return Scaffold(
             appBar: AppBar(
-              elevation: 1,
+              elevation: 0,
             ),
             backgroundColor: mainTheme,
             body: Padding(
-              padding: const EdgeInsets.all(35),
+              padding: const EdgeInsets.only(left: 35, right: 35),
               child: Column(
                 children: <Widget>[
-                  Text(_userModel.fullName,
-                      style: TextStyle(color: Colors.white)),
-                  Text(widget.artist.name,
-                      style: TextStyle(color: Colors.white)),
+                  _ArtistHeader(
+                      widget.artist.name, widget.artist.images.first.url),
+                  _ArtistTracks(),
                 ],
               ),
             ),
@@ -45,5 +44,60 @@ class _ArtistScreenState extends State<ArtistScreen> {
         },
       ),
     );
+  }
+}
+
+class _ArtistHeader extends StatefulWidget {
+  final String artist;
+  final String imgUrl;
+
+  const _ArtistHeader(this.artist, this.imgUrl);
+
+  @override
+  __ArtistHeaderState createState() => __ArtistHeaderState();
+}
+
+class __ArtistHeaderState extends State<_ArtistHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            color: Colors.white,
+            borderRadius: BorderRadius.all(const Radius.circular(200)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 0.5,
+                blurRadius: 5,
+                offset: Offset(1, 1),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(200),
+            child: Image.network(widget.imgUrl),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(widget.artist),
+      ],
+    );
+  }
+}
+
+class _ArtistTracks extends StatefulWidget {
+  @override
+  __ArtistTracksState createState() => __ArtistTracksState();
+}
+
+class __ArtistTracksState extends State<_ArtistTracks> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
