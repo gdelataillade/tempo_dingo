119e74bedb7326da04568c59d1772316d79be074
Gautier <gautier2406@gmail.com>
2020-04-15 14:24:40 +0200

    Config: Add some default tracks

--- lib/src/config/register_config.dart
+++ lib/src/config/register_config.dart
@@ -9,12 +9,18 @@ final Map<String, List<String>> library = {
     "45KqDHTOJtIM53QR1gPGTa", // Brothers
     "51R2M1JgyFfRS3e6v5wCt3", // Clandestino
     "6FNdxaAOler5LutmU2sVxT", // L'hymne de nos campagnes 2019
+    "40riOy7x9W7GXjyGp4pjAv", // Hotel California
+    "3d9DChrdc6BOeFsbrZ3Is0", // Under the Bridge
+    "2Foc5Q5nqNiosCNqttzHof", // Get Lucky
   ],
   "artists": [
     "776Uo845nYHJpNaStv1Ds4", // Jimi Hendrix
     "6pdcQa7by8IKuoVXvgknlI", // Riles
     "6wH6iStAh4KIaWfuhf0NYM", // Manu Chao
     "4ZP3CHz28XWVLGiaVVWuLj", // Tryo
+    "0ECwFtbIWEVNwjlrfc6xoL", // Eagles
+    "0L8ExT028jH3ddEcZwqJJ5", // Red Hot Chili Peppers
+    "4tZwfgrHOc3mvqYlEYSvVi", // Daft Punk
   ],
   "favorite": [],
   "history": [],
