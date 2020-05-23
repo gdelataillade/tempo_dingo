4cf1b71feaec4048d3febf5d53c516176b0216b5
Gautier <gautier2406@gmail.com>
2020-05-23 20:18:10 +0200

    Game: Center album image

--- lib/src/screens/game.dart
+++ lib/src/screens/game.dart
@@ -252,18 +252,20 @@ class __AlbumProgressiveBarState extends State<_AlbumProgressiveBar>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 300,
-      child: CustomPaint(
-        foregroundPainter: _MyPainter(
-          completePercent: _percentage,
-          width: 3.0,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(300)),
-          child: Image.network(widget.track.album.images.first.url,
-              width: MediaQuery.of(context).size.width),
+    return Center(
+      child: Container(
+        width: 300,
+        height: 300,
+        child: CustomPaint(
+          foregroundPainter: _MyPainter(
+            completePercent: _percentage,
+            width: 3.0,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(300)),
+            child: Image.network(widget.track.album.images.first.url,
+                width: MediaQuery.of(context).size.width),
+          ),
         ),
       ),
     );
@@ -480,39 +482,17 @@ class _LineChartSample2State extends State<LineChartSample2> {
             aspectRatio: 1.70,
             child: Container(
               decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(18),
-                ),
-                // color: Color(0xff232d37),
+                borderRadius: BorderRadius.all(Radius.circular(18)),
               ),
               child: Padding(
                 padding: const EdgeInsets.only(
                     right: 18.0, left: 12.0, top: 24, bottom: 12),
                 child: LineChart(
                   mainData(),
-                  // showAvg ? avgData() : mainData(),
                 ),
               ),
             ),
           ),
-          // SizedBox(
-          //   width: 60,
-          //   height: 34,
-          //   child: FlatButton(
-          //     onPressed: () {
-          //       setState(() {
-          //         showAvg = !showAvg;
-          //       });
-          //     },
-          //     child: Text(
-          //       'avg',
-          //       style: TextStyle(
-          //           fontSize: 12,
-          //           color:
-          //               showAvg ? Colors.white.withOpacity(0.5) : Colors.white),
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
@@ -603,102 +583,6 @@ class _LineChartSample2State extends State<LineChartSample2> {
       ],
     );
   }
-
-  // LineChartData avgData() {
-  //   return LineChartData(
-  //     lineTouchData: LineTouchData(enabled: false),
-  //     gridData: FlGridData(
-  //       show: true,
-  //       drawHorizontalLine: true,
-  //       getDrawingVerticalLine: (value) {
-  //         return FlLine(
-  //           color: const Color(0xff37434d),
-  //           strokeWidth: 1,
-  //         );
-  //       },
-  //       getDrawingHorizontalLine: (value) {
-  //         return FlLine(
-  //           color: const Color(0xff37434d),
-  //           strokeWidth: 1,
-  //         );
-  //       },
-  //     ),
-  //     titlesData: FlTitlesData(
-  //       show: true,
-  //       bottomTitles: SideTitles(
-  //         showTitles: true,
-  //         reservedSize: 22,
-  //         textStyle: const TextStyle(
-  //             color: Color(0xff68737d),
-  //             fontWeight: FontWeight.bold,
-  //             fontSize: 16),
-  //         getTitles: (value) {
-  //           switch (value.toInt()) {
-  //             case 0:
-  //               return '0s';
-  //             case 5:
-  //               return '15s';
-  //             case 10:
-  //               return '30s';
-  //           }
-  //           return '';
-  //         },
-  //         margin: 8,
-  //       ),
-  //       leftTitles: SideTitles(
-  //         showTitles: true,
-  //         textStyle: const TextStyle(
-  //           color: Color(0xff67727d),
-  //           fontWeight: FontWeight.bold,
-  //           fontSize: 15,
-  //         ),
-  //         getTitles: (value) {
-  //           switch (value.toInt()) {
-  //             case 5:
-  //               return '50%';
-  //             case 10:
-  //               return '100%';
-  //           }
-  //           return '';
-  //         },
-  //         reservedSize: 28,
-  //         margin: 12,
-  //       ),
-  //     ),
-  //     borderData: FlBorderData(
-  //         show: true,
-  //         border: Border.all(color: const Color(0xff37434d), width: 1)),
-  //     minX: 0,
-  //     maxX: 10,
-  //     minY: 0,
-  //     maxY: 10,
-  //     lineBarsData: [
-  //       LineChartBarData(
-  //         spots: _buildSpots(),
-  //         isCurved: true,
-  //         colors: [
-  //           ColorTween(begin: gradientColors[0], end: gradientColors[1])
-  //               .lerp(0.2),
-  //           ColorTween(begin: gradientColors[0], end: gradientColors[1])
-  //               .lerp(0.2),
-  //         ],
-  //         barWidth: 5,
-  //         isStrokeCapRound: true,
-  //         dotData: FlDotData(
-  //           show: false,
-  //         ),
-  //         belowBarData: BarAreaData(show: true, colors: [
-  //           ColorTween(begin: gradientColors[0], end: gradientColors[1])
-  //               .lerp(0.2)
-  //               .withOpacity(0.1),
-  //           ColorTween(begin: gradientColors[0], end: gradientColors[1])
-  //               .lerp(0.2)
-  //               .withOpacity(0.1),
-  //         ]),
-  //       ),
-  //     ],
-  //   );
-  // }
 }
 
 class _MyPainter extends CustomPainter {
